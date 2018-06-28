a617e264ff7a902771515201e25565db0e7cf53b
Onur Ağtaş <agtasonur@gmail.com>
2018-06-28 14:57:45 +0300

    gui width, height

--- Ui_gui.py
+++ Ui_gui.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(696, 192)
+        Dialog.resize(316, 295)
         self.gridLayout = QtWidgets.QGridLayout(Dialog)
         self.gridLayout.setObjectName("gridLayout")
         self.devicesList = QtWidgets.QListWidget(Dialog)
